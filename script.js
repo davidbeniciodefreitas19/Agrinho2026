95e468a0007a9d896decd2bc3346dfe69fa16a42
davidbeniciodefreitas19 <david.beniciode.freitas@escola.pr.gov.br>
2026-06-29 16:26:19 -0300

    Add functions to modify text size and greet user

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+let tamanho = 16;
 
+function entrar(){
+
+    let nome = document.getElementById("nome").value;
+
+    document.getElementById("mensagem").innerHTML =
+    "Oi, " + nome + "!";
+}
+
+function aumentar(){
+
+    tamanho = tamanho + 2;
+
+    document.getElementById("texto").style.fontSize = tamanho + "px";
+
+}
+
+function diminuir(){
+
+    tamanho = tamanho - 2;
+
+    document.getElementById("texto").style.fontSize = tamanho + "px";
+
+}
